e9ef1d40b70cde7411b3ee8beea7bf33331e8241
Igor Cafe <igorcafe@protonmail.com>
2024-03-24 23:16:58 -0300

    graceful shutdown, refactor, comments

--- nanoarch2.c
+++ nanoarch2.c
@@ -6,82 +6,93 @@
 #include <errno.h>
 #include "libretro.h"
 #include <alsa/asoundlib.h>
+#include <signal.h>
 
 #define fatal(msg, ...)                      \
     {                                        \
-    fprintf(stderr, "FATAL: ");          \
-    fprintf(stderr, msg, ##__VA_ARGS__); \
-    fprintf(stderr, "\n");               \
-    shutdown();                          \
-        exit(1);                             \
+        fprintf(stderr, "FATAL: ");          \
+        fprintf(stderr, msg, ##__VA_ARGS__); \
+        fprintf(stderr, "\n");               \
+        shutdown(1);                         \
     }
 
-#define load_sym(H, N)                                          \
+// loads a symbol named N from handle H inside struct B
+#define load_sym(H, B, N)                                       \
     {                                                           \
-    (*(void **)&N) = dlsym(H, #N);                          \
-    if (!N)                                                 \
-    {                                                       \
-        fatal("failed to load symbol '#N': %s", dlerror()); \
+        (*(void **)&B.N) = dlsym(H, #N);                        \
+        if (!N)                                                 \
+        {                                                       \
+            fatal("failed to load symbol '#N': %s", dlerror()); \
         }                                                       \
     }
 
-// #define DEBUG 1
-#ifdef DEBUG
-#define initscr() 0
-#define endwin() 0
-#define mvprintw(a, b, c) 0
-#define timeout(x) 0
-#define getch() 0
-#define refresh() 0
-#define attron(x) 0
-#define attroff(x) 0
-#define COLOR_RED 0
-#define COLOR_GREEN 0
-#define COLOR_BLUE 0
-#define KEY_ENTER 1
-#endif
-
 struct g_app
 {
-    // WINDOW *win;
+    // used to play sound
     snd_pcm_t *pcm;
+
+    // indicates the state of each button in the retropad
     unsigned joypad[RETRO_DEVICE_ID_JOYPAD_L3 + 1];
-    unsigned enter;
+
+    // libretro functions
+    void (*retro_set_environment)(retro_environment_t);
+    void (*retro_set_video_refresh)(retro_video_refresh_t);
+    void (*retro_set_audio_sample)(retro_audio_sample_t);
+    void (*retro_set_audio_sample_batch)(retro_audio_sample_batch_t);
+    void (*retro_set_input_poll)(retro_input_poll_t);
+    void (*retro_set_input_state)(retro_input_state_t);
+    void (*retro_init)(void);
+    void (*retro_deinit)(void);
+    unsigned (*retro_api_version)(void);
+    void (*retro_get_system_info)(struct retro_system_info *);
+    void (*retro_get_system_av_info)(struct retro_system_av_info *);
+    void (*retro_set_controller_port_device)(unsigned, unsigned);
+    void (*retro_reset)(void);
+    void (*retro_run)(void);
+    size_t (*retro_serialize_size)(void);
+    bool (*retro_serialize)(void *, size_t);
+    bool (*retro_unserialize)(const void *, size_t);
+    bool (*retro_load_game)(const struct retro_game_info *);
+    void (*retro_unload_game)(void);
 } g = {0};
 
-void shutdown()
+void shutdown(int status)
 {
     if (g.pcm)
-    {
         snd_pcm_close(g.pcm);
-    }
 
-    // if (g.win)
-    // {
-    //     delwin(g.win);
-    // }
+    if (g.retro_unload_game)
+        g.retro_unload_game();
+
+    if (g.retro_deinit)
+        g.retro_deinit();
 
     endwin();
+
+    fprintf(stderr, "exited\n");
+    fflush(stderr);
+
+    exit(status);
 }
 
-bool set_environment(unsigned cmd, void *data)
+// TODO
+bool cb_environment(unsigned cmd, void *data)
 {
     return 0;
 }
 
-void set_video_refresh(const void *data, unsigned width, unsigned height, size_t pitch)
+// Currently only supports 0RGB155 pixel format, where each pixels contain 16 bit of information:
+// ARRRRRGGGGGBBBBB
+// A (alpha):  0 = transparent, 1 = opaque
+// R (red): 5 bit unsigned number, representing the intensity of read (0 = no red, 31 = max red)
+// G (green): same idea as R
+// B (blue): same idea as R
+void cb_video_refresh(const void *data, unsigned width, unsigned height, size_t pitch)
 {
     if (!data)
-    {
         return;
-    }
 
     uint16_t *pixels = (uint16_t *)data;
-
-    start_color();
-    init_pair(COLOR_RED, COLOR_RED, COLOR_RED);
-    init_pair(COLOR_GREEN, COLOR_GREEN, COLOR_GREEN);
-    init_pair(COLOR_BLUE, COLOR_BLUE, COLOR_BLUE);
 
     int step = 2;
     int skip = 0;
@@ -90,61 +101,47 @@ void set_video_refresh(const void *data, unsigned width, unsigned height, size_t
         unsigned w = (i - skip) % width / step;
         unsigned h = (i - skip) / width / step;
 
-        // ARRRRRGGGGBBBBB
-
         uint16_t a = (pixels[i] >> 14) & 1;
-        // tranparent
+        // alpha = 0, draw transparent block
         if (!a)
         {
-            // wmove(g.win, h, w);
-            // wprintw(g.win, " ");
             mvprintw(h, w, " ");
             continue;
         }
 
+        // just taking the bits from 0RGB1555 as explained before
         uint16_t red = (pixels[i] >> 9) & 0x1F;
         uint16_t green = (pixels[i] >> 6) & 0x1F;
         uint16_t blue = pixels[i] & 0x1F;
 
         unsigned color = 0;
         if (red > green && red > blue)
-        {
             color = COLOR_RED;
-        }
         if (green > red && green > blue)
-        {
             color = COLOR_GREEN;
-        }
         if (blue > red && blue > green)
-        {
             color = COLOR_BLUE;
-        }
 
+        // color not identified, draw transparent block
         if (!color)
         {
-            // wmove(g.win, h, w);
-            // wprintw(g.win, " ");
             mvprintw(h, w, " ");
             continue;
         }
 
+        // draw pixel
         attron(COLOR_PAIR(color));
-        // wmove(g.win, h, w);
-        // wprintw(g.win, " ");
         mvprintw(h, w, " ");
         attroff(COLOR_PAIR(color));
     }
 
-    // wrefresh(g.win);
     refresh();
 }
 
-size_t set_audio_sample_batch(const int16_t *data, size_t frames)
+size_t cb_audio_sample_batch(const int16_t *data, size_t frames)
 {
     if (!g.pcm)
-    {
         return 0;
-    }
 
     int n = snd_pcm_writei(g.pcm, data, frames);
     if (n < 0)
@@ -156,19 +153,18 @@ size_t set_audio_sample_batch(const int16_t *data, size_t frames)
     return n;
 }
 
-void set_audio_sample(int16_t left, int16_t right)
+void cb_audio_sample(int16_t left, int16_t right)
 {
     int16_t buf[2] = {left, right};
-    set_audio_sample_batch(buf, 1);
+    cb_audio_sample_batch(buf, 1);
 }
 
-void set_input_poll(void)
+// input poll captures and stores input state
+void cb_input_poll(void)
 {
     int ch = getch();
     if (ch == ERR)
-    {
         return;
-    }
 
     if (ch == 'j')
         g.joypad[RETRO_DEVICE_ID_JOYPAD_LEFT] = 1;
@@ -206,83 +202,76 @@ void set_input_poll(void)
         g.joypad[RETRO_DEVICE_ID_JOYPAD_START] = 0;
 }
 
-int16_t set_input_state(unsigned port, unsigned device, unsigned index, unsigned id)
+// core calls this function to get the state of a specific button for example.
+// port 0 is the console port for player 1.
+// index is basically only used for analogs, where 0 is left analog and 1 is right analog.
+// since it only supports keyboard it is always treated as 0.
+// device is the emulated device and can be a mouse, keyboard, retropad, analog, light gun, ...
+int16_t cb_input_state(unsigned port, unsigned device, unsigned index, unsigned id)
 {
     if (port || index || device != RETRO_DEVICE_JOYPAD)
-    {
         return 0;
-    }
 
     return g.joypad[id];
 }
 
+void signal_handler(int i)
+{
+    fatal("interruped: shutting down");
+}
+
 int main(int argc, char *argv[])
 {
+    signal(SIGINT, signal_handler);
+    signal(SIGKILL, signal_handler);
 
     if (argc != 3)
-        fatal("usage: %s <corepath> <rompath>", argv[0]);
+        fatal("usage: %s <corepath> <rompath> (only %d args given)", argv[0], argc);
 
-    void *handle = dlopen(argv[1], RTLD_LAZY);
+    char *core_path = argv[1];
+    char *rom_path = argv[2];
+
+    // try to dynamically link with core
+    void *handle = dlopen(core_path, RTLD_LAZY);
     if (!handle)
-        fatal("failed to load core %s: %s", argv[1], dlerror());
+        fatal("failed to load core %s: %s", core_path, dlerror());
 
-    void (*retro_set_environment)(retro_environment_t);
-    void (*retro_set_video_refresh)(retro_video_refresh_t);
-    void (*retro_set_audio_sample)(retro_audio_sample_t);
-    void (*retro_set_audio_sample_batch)(retro_audio_sample_batch_t);
-    void (*retro_set_input_poll)(retro_input_poll_t);
-    void (*retro_set_input_state)(retro_input_state_t);
+    load_sym(handle, g, retro_set_environment);
+    load_sym(handle, g, retro_set_video_refresh);
+    load_sym(handle, g, retro_set_audio_sample);
+    load_sym(handle, g, retro_set_audio_sample_batch);
+    load_sym(handle, g, retro_set_input_poll);
+    load_sym(handle, g, retro_set_input_state);
+    load_sym(handle, g, retro_init);
+    load_sym(handle, g, retro_deinit);
+    load_sym(handle, g, retro_api_version);
+    load_sym(handle, g, retro_get_system_info);
+    load_sym(handle, g, retro_get_system_av_info);
+    load_sym(handle, g, retro_set_controller_port_device);
+    load_sym(handle, g, retro_reset);
+    load_sym(handle, g, retro_run);
+    load_sym(handle, g, retro_serialize_size);
+    load_sym(handle, g, retro_serialize);
+    load_sym(handle, g, retro_unserialize);
+    load_sym(handle, g, retro_load_game);
+    load_sym(handle, g, retro_unload_game);
 
-    void (*retro_init)(void) = NULL;
-    void (*retro_deinit)(void) = NULL;
-    unsigned (*retro_api_version)(void) = NULL;
-    void (*retro_get_system_info)(struct retro_system_info *) = NULL;
-    void (*retro_get_system_av_info)(struct retro_system_av_info *) = NULL;
-    void (*retro_set_controller_port_device)(unsigned, unsigned) = NULL;
-    void (*retro_reset)(void) = NULL;
-    void (*retro_run)(void) = NULL;
-    size_t (*retro_serialize_size)(void) = NULL;
-    bool (*retro_serialize)(void *, size_t) = NULL;
-    bool (*retro_unserialize)(const void *, size_t) = NULL;
-    bool (*retro_load_game)(const struct retro_game_info *) = NULL;
-    void (*retro_unload_game)(void) = NULL;
-
-    load_sym(handle, retro_set_environment);
-    load_sym(handle, retro_set_video_refresh);
-    load_sym(handle, retro_set_audio_sample);
-    load_sym(handle, retro_set_audio_sample_batch);
-    load_sym(handle, retro_set_input_poll);
-    load_sym(handle, retro_set_input_state);
-
-    load_sym(handle, retro_init);
-    load_sym(handle, retro_deinit);
-    load_sym(handle, retro_api_version);
-    load_sym(handle, retro_get_system_info);
-    load_sym(handle, retro_get_system_av_info);
-    load_sym(handle, retro_set_controller_port_device);
-    load_sym(handle, retro_reset);
-    load_sym(handle, retro_run);
-    load_sym(handle, retro_serialize_size);
-    load_sym(handle, retro_serialize);
-    load_sym(handle, retro_unserialize);
-    load_sym(handle, retro_load_game);
-    load_sym(handle, retro_unload_game);
-
-    retro_set_environment(set_environment);
-    retro_set_video_refresh(set_video_refresh);
-    retro_set_audio_sample(set_audio_sample);
-    retro_set_audio_sample_batch(set_audio_sample_batch);
-    retro_set_input_poll(set_input_poll);
-    retro_set_input_state(set_input_state);
+    // register callbacks
+    g.retro_set_environment(cb_environment);
+    g.retro_set_video_refresh(cb_video_refresh);
+    g.retro_set_audio_sample(cb_audio_sample);
+    g.retro_set_audio_sample_batch(cb_audio_sample_batch);
+    g.retro_set_input_poll(cb_input_poll);
+    g.retro_set_input_state(cb_input_state);
 
     // load core
-    retro_init();
+    g.retro_init();
     fprintf(stderr, "core loaded\n");
 
-    // load game
-    struct retro_game_info game = {argv[2], 0};
+    // gather game info
+    struct retro_game_info game = {rom_path, 0};
 
-    FILE *file = fopen(argv[2], "rb");
+    FILE *file = fopen(rom_path, "rb");
     if (!file)
         fatal("failed to open rom: %s", strerror(errno));
 
@@ -291,7 +280,7 @@ int main(int argc, char *argv[])
     rewind(file);
 
     struct retro_system_info system = {0};
-    retro_get_system_info(&system);
+    g.retro_get_system_info(&system);
 
     if (!system.need_fullpath)
     {
@@ -303,12 +292,12 @@ int main(int argc, char *argv[])
             fatal("failed to read game rom: %s", strerror(errno));
     }
 
-    if (!retro_load_game(&game))
+    if (!g.retro_load_game(&game))
         fatal("core failed to load game");
 
     // init sound
     struct retro_system_av_info av = {0};
-    retro_get_system_av_info(&av);
+    g.retro_get_system_av_info(&av);
 
     int err = snd_pcm_open(&g.pcm, "default", SND_PCM_STREAM_PLAYBACK, 0);
     if (err < 0)
@@ -319,6 +308,10 @@ int main(int argc, char *argv[])
         fatal("failed to configure playback device: %s", snd_strerror(err));
 
     // init window
+    start_color();
+    init_pair(COLOR_RED, COLOR_RED, COLOR_RED);
+    init_pair(COLOR_GREEN, COLOR_GREEN, COLOR_GREEN);
+    init_pair(COLOR_BLUE, COLOR_BLUE, COLOR_BLUE);
     initscr();
     cbreak();
     keypad(stdscr, TRUE);
@@ -326,7 +319,7 @@ int main(int argc, char *argv[])
 
     // main loop
     for (;;)
-        retro_run();
+        g.retro_run();
 
-    shutdown();
+    shutdown(0);
 }
